fdb0314df42ce0d9dbcb2cc4f902e8c83cf21dff
MaxBerezhnoi <92980454+MaxBerezhnoi@users.noreply.github.com>
2022-06-04 21:07:17 +0300

    запросы и разметка карточки

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 import './sass/main.scss';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import pictureCard from './templates/pictureCard.hbs';
+/*let a = "red woman"
+fetch(`https://pixabay.com/api/?key=27845573-c4cb755b67afa71dbf7307045&q=${a}`).then(response => {
+    return response.json();
+}).then(console.log);*/
 
-console.log('Hello World');
+const gallery = document.querySelector(".gallery");
+const form = document.querySelector('.search-form');
+form.addEventListener("submit", fetchPicture);
+
+function fetchPicture(e) {
+    //let name = e.target.value.trim();
+    e.preventDefault();
+    const input = document.querySelector("input");
+    console.log(input.value.trim());
+    return fetch(`https://pixabay.com/api/?key=27845573-c4cb755b67afa71dbf7307045&q=${input.value.trim()}&image_type=photo&orientation=horizontal&safesearch=true`)
+    .then(response => {
+                return response.json();
+    }).then(viewGallery => {
+        console.log(viewGallery);
+        if (viewGallery.total >= 1) {
+            const markup = pictureCard(viewGallery);
+            console.log(markup);
+            gallery.innerHTML = markup;
+            
+        }
+        else {
+            Notify.info("Sorry, there are no images matching your search query. Please try again.");  
+        }
+    }).catch(error => {
+        console.log(error);
+    })
+            
+}
+        
+
+
+
