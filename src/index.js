815eacb54e0a3bf780ea6ce24860f2776a3c4e79
MaxBerezhnoi <92980454+MaxBerezhnoi@users.noreply.github.com>
2022-06-05 10:55:58 +0300

    настройка пагинации

--- src/index.js
+++ src/index.js
@@ -5,22 +5,42 @@ import pictureCard from './templates/pictureCard.hbs';
 fetch(`https://pixabay.com/api/?key=27845573-c4cb755b67afa71dbf7307045&q=${a}`).then(response => {
     return response.json();
 }).then(console.log);*/
-
+const loadMore = document.querySelector(".load-more");
+loadMore.addEventListener("click", loadMoreFu);
+let page = 1;
 const gallery = document.querySelector(".gallery");
+
 const form = document.querySelector('.search-form');
 form.addEventListener("submit", fetchPicture);
 
+const input = document.querySelector("input");
+    input.addEventListener("input", cleanFu);
+function loadMoreFu(e) {
+    page += 1;
+    fetchPicture(e);
+}
+
+function cleanFu(e) {
+   if (input.value.trim() === "") {
+            page = 1;
+            gallery.innerHTML = "";
+            return page;
+        } 
+}
+
 function fetchPicture(e) {
-    //let name = e.target.value.trim();
     e.preventDefault();
-    const input = document.querySelector("input");
+    
+
+    
     console.log(input.value.trim());
-    return fetch(`https://pixabay.com/api/?key=27845573-c4cb755b67afa71dbf7307045&q=${input.value.trim()}&image_type=photo&orientation=horizontal&safesearch=true`)
+    return fetch(`https://pixabay.com/api/?key=27845573-c4cb755b67afa71dbf7307045&q=${input.value.trim()}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`)
     .then(response => {
                 return response.json();
     }).then(viewGallery => {
         console.log(viewGallery);
-        if (viewGallery.total >= 1) {
+        //-input if
+         if (viewGallery.total >= 1) {
             Notify.success(`Hooray! We found ${viewGallery.total} images.`);
             const markup = pictureCard(viewGallery);
             console.log(markup);
