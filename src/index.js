441265c1db415f9157dfdfb2165672b774129818
MaxBerezhnoi <92980454+MaxBerezhnoi@users.noreply.github.com>
2022-06-06 19:52:59 +0300

    сообщения

--- src/index.js
+++ src/index.js
@@ -42,13 +42,22 @@ function fetchPicture(e) {
         console.log(viewGallery);
         //-input if
          if (viewGallery.total >= 1) {
-             Notify.success(`Hooray! We found ${viewGallery.total} images.`);
-             const q = page * 40;
-             Notify.info(`You see ${q} fotos`);
+            Notify.success(`Hooray! We found ${viewGallery.total} images.`);
+            const q = viewGallery.hits.length;
+             const qQ = q * page;
+             
             const markup = pictureCard(viewGallery);
             console.log(markup);
              gallery.innerHTML = markup;
              loadMore.removeAttribute("hidden");
+             if (q < 40) {
+                 loadMore.setAttribute("hidden", true);
+                 Notify.info(`We're sorry, but you've reached the end of search results. It was the last ${q} pictures`);
+                  
+             }
+             else if (q === 40) (
+                 Notify.info(`You see ${qQ} pictures`)
+             )
             
         }
         else {
